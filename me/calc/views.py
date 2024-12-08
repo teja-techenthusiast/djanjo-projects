df61454186454939050853a8b889b78df4c22583
Teja Chamakuri <tejach312@gmail.com>
2024-12-08 12:40:35 +0530

    added static files

--- me/calc/views.py
+++ me/calc/views.py
@@ -5,4 +5,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html',{'name':"Teja"})
+    return render(request, 'home.html', {'name': "Teja"})
+
+
+def add(request):
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    res = val1 + val2
+    return render(request, "result.html" , {'result':res})
