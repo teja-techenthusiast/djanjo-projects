18210acb98ac466986def608efc7d6cf5ca28c1c
Teja Chamakuri <tejach312@gmail.com>
2024-12-12 19:36:49 +0530

    added database to the project

--- me/me/settings.py
+++ me/me/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig' ,
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'me.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'teja',
+        'USER' : 'postgres',
+        'PASSWORD' :'1234' ,
+        'HOST' : 'localhost'
     }
 }
 
--- me/travello/models.py
+++ me/travello/models.py
@@ -3,13 +3,14 @@ from django.db import models
 
 
 # Create your models here.
-class Destination:
-    id : int
-    name : str
-    img : str
-    desc : str
-    price : int
-    offer : bool
+class Destination(models.Model): #converting class into model
+    #creating columns
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
+
 
 
 
