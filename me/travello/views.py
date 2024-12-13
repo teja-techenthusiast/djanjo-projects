c05637e8b836bbc68883dad5aa508164d6b85310
Teja Chamakuri <tejach312@gmail.com>
2024-12-13 21:51:57 +0530

    added and fetched data from database

--- me/travello/views.py
+++ me/travello/views.py
@@ -4,28 +4,9 @@ from .models import Destination
 # Create your views here.
 def index(request):
 
-    dest1 = Destination()
-    dest1.name = 'Mumbai'
-    dest1.desc = 'The city that never sleeps'
-    dest1.img = 'destination_1.jpg'
-    dest1.price = 700
-    dest1.offer = False
 
-    dest2 = Destination()
-    dest2.name = 'Hyderabad'
-    dest2.desc = 'Biryani then sharwani'
-    dest2.img = 'destination_2.jpg'
-    dest2.price = 800
-    dest2.offer = True
+    dests = Destination.objects.all()
 
-    dest3 = Destination()
-    dest3.name = 'Bengaluru'
-    dest3.desc = 'Beautiful city'
-    dest3.img = 'destination_3.jpg'
-    dest3.price = 900
-    dest3.offer = True
-
-    dests =[dest1, dest2, dest3]
 
 
     return render(request, "index.html", {'dests':dests})
